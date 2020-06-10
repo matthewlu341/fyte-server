19c21367a01fc4db9179b84a40b148ef254d44f6
Matthew <matt.lu@mail.utoronto.ca>
2020-06-10 14:36:29 -0400

    new buildpack and args

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.listen ( process.env.PORT || 3001, ()=>{
 })
 
 async function scrape(){
-    let browser = await puppeteer.launch(),
+    let browser = await puppeteer.launch({ args: ['--no-sandbox'] }),
     page = await browser.newPage();
     await page.goto('https://sportsurge.net/#/groups/19' , {
         waitUntil: 'networkidle0',
