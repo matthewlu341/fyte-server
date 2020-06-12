fb5995a4ec82d162f51141c3add7b2ba582c65a6
Matthew <matt.lu@mail.utoronto.ca>
2020-06-12 15:25:07 -0400

    twitter key hidden

--- server.js
+++ server.js
@@ -79,10 +79,10 @@ app.get('/', (req, res) => {
 app.get('/tweets', (req, res)=>{
         var Twit = require('twit')
         var T = new Twit({
-        consumer_key:         's5TtlAgj746Spby4Nx7DOryyU',
-        consumer_secret:      'Sw68vQWuPuHcBKAGnK17DJxDMduQu6pjBybYG6as1lNTBolsxo',
-        access_token:         '2991041913-lQt07ijIbgF9FhWWJkIKAe7wrqdNhz3KteLy5R3',
-        access_token_secret:  'UGpPNqWm9xupXC15huQmyd7NWADxVPglfC8Hlo3BD10Vz',
+        consumer_key:        `${process.env.CONSUMER_KEY}`,
+        consumer_secret:     `${process.env.CONSUMER_SECRET}`,
+        access_token:         `${process.env.ACCESS_TOKEN}`,
+        access_token_secret:  `${process.env.ACCESS_TOKEN_SECRET}`,
         strictSSL:            true,     // optional - requires SSL certificates to be valid.
         })
         var data = T.get('statuses/user_timeline', { user_id: '	6446742' }, (err,data,response) => {
